bd0ed221fc371622f2c7763544fe5637172614ae
user <user@user-pc>
2018-09-06 15:31:11 +0900

    되나? 되겠지?

--- catch_myproject/src/com/sist/common/Function.java
+++ catch_myproject/src/com/sist/common/Function.java
@@ -1,6 +1,6 @@
 package com.sist.common; 
-//LOGIN|aaaaaaa
-//out.write(("100|id|pwd"))
+//LOGIN|aaaaaaaa
+//out.write(("100|id|pwd")) 
 //out.write(("LOGIN|id|pwd"))
 public class Function {
 	//public static final 데이터형 변수명 = 상수; // 설명
